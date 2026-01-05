840c4d70ea812dd9bf739f8f3c5853fa1e9d58ba
sukkalajayanth <sukkala.jayanth@gmail.com>
2026-01-05 21:43:19 +0530

    Initialize VM state and memory

--- vm.c
+++ vm.c
@@ -1,1 +1,12 @@
 #include "vm.h"
+
+void init_vm(VM *vm, int *code, int size) {
+    vm->sp = -1;
+    vm->pc = 0;
+    vm->code = code;
+    vm->code_size = size;
+
+    for (int i = 0; i < MEM_SIZE; i++) {
+        vm->memory[i] = 0;
+    }
+}
