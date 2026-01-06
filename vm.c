674e0c6f2a0223bf3fa3b6fa4ad6f1e2d7ea9c62
sukkalajayanth <sukkala.jayanth@gmail.com>
2026-01-06 22:17:03 +0530

    Add stack operations and arithmetic instructions

--- vm.c
+++ vm.c
@@ -1,12 +1,51 @@
 #include "vm.h"
+#include <stdio.h>
 
-void init_vm(VM *vm, int *code, int size) {
-    vm->sp = -1;
-    vm->pc = 0;
-    vm->code = code;
-    vm->code_size = size;
+enum {
+    HALT,
+    PUSH,
+    POP,
+    ADD,
+    SUB,
+    MUL,
+    DIV
+};
 
-    for (int i = 0; i < MEM_SIZE; i++) {
-        vm->memory[i] = 0;
+void run_vm(VM *vm) {
+    while (vm->pc < vm->code_size) {
+        int opcode = vm->code[vm->pc++];
+
+        switch (opcode) {
+            case PUSH:
+                vm->stack[++vm->sp] = vm->code[vm->pc++];
+                break;
+
+            case POP:
+                vm->sp--;
+                break;
+
+            case ADD:
+                vm->stack[vm->sp - 1] += vm->stack[vm->sp];
+                vm->sp--;
+                break;
+
+            case SUB:
+                vm->stack[vm->sp - 1] -= vm->stack[vm->sp];
+                vm->sp--;
+                break;
+
+            case MUL:
+                vm->stack[vm->sp - 1] *= vm->stack[vm->sp];
+                vm->sp--;
+                break;
+
+            case DIV:
+                vm->stack[vm->sp - 1] /= vm->stack[vm->sp];
+                vm->sp--;
+                break;
+
+            case HALT:
+                return;
+        }
     }
 }
