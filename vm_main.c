5fbe472734683be9c4e9f3f5434045df82162a60
sukkalajayanth <sukkala.jayanth@gmail.com>
2026-01-08 13:30:59 +0530

    Add VM execution driver

--- vm_main.c
+++ vm_main.c
@@ -1,3 +1,18 @@
+#include "vm.h"
+#include <stdio.h>
+
 int main() {
+    int code[] = {
+        PUSH, 10,
+        PUSH, 20,
+        ADD,
+        HALT
+    };
+
+    VM vm;
+    init_vm(&vm, code, sizeof(code) / sizeof(int));
+    run_vm(&vm);
+
+    printf("Result: %d\n", vm.stack[vm.sp]);
     return 0;
 }
